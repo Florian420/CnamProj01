24e8589b1842b07f04dbe8a9a28129d19841628a
gfayet69 <84783502+gfayet69@users.noreply.github.com>
2021-05-26 09:16:54 +0200

    Update init_db.sql

--- init_db.sql
+++ init_db.sql
@@ -1,57 +1,57 @@
-CREATE DATABASE IF NOT EXISTS quiz
+CREATE DATABASE IF NOT EXISTS QuizzIT;
 CHARACTER SET = 'utf8'
 COLLATE = 'utf8_general_ci';
 
-USE quiz;
+CREATE TABLE Theme
+(idTheme int  not null  AUTO_INCREMENT,
+libelleTheme varchar(50) not null,
+primary key (idTheme));
 
-CREATE TABLE IF NOT EXISTS user(
-						user_id INT PRIMARY KEY AUTO_INCREMENT,
-						user_firstname VARCHAR(32),
-						user_name VARCHAR(32),
-						user_login VARCHAR(16) NOT NULL UNIQUE,
-						user_passwd VARCHAR(512) NOT NULL,
-						user_rank INT
-						) ENGINE = InnoDB;
+CREATE TABLE Module
+(idModule int not null AUTO_INCREMENT,
+libelleModule varchar(50) not null,
+nbQuestion int not null,
+scoreTotal int,
+primary key (idModule),
+idTheme int not null,
+foreign key (idTheme) references Theme(idTheme) );
 
-CREATE TABLE IF NOT EXISTS module(
-						mod_id INT PRIMARY KEY AUTO_INCREMENT,
-						mod_level INT,
-						mod_title VARCHAR(32),
-						mod_name VARCHAR(32),
-						mod_desc VARCHAR(255)
-						) ENGINE = InnoDB;
+CREATE TABLE Question
+(idQuestion int not null AUTO_INCREMENT,
+libelleQuestion varchar(150) not null,
+primary key (idQuestion),
+idModule int not null,
+foreign key (idModule) references Module(idModule));
 
-CREATE TABLE IF NOT EXISTS question(
-						q_id INT PRIMARY KEY AUTO_INCREMENT,
-						mod_id INT,
-						q_text VARCHAR(255),
-						CONSTRAINT fk_question_mod FOREIGN KEY(mod_id) REFERENCES module(mod_id)
-						) ENGINE = InnoDB;
+CREATE TABLE Utilisateur
+(idUtilisateur int not null AUTO_INCREMENT,
+nom varchar(50) not null,
+prenom varchar(50) not null,
+login varchar(50) not null,
+mdp varchar(50) not null,
+admin boolean,
+primary key (idUtilisateur));
 
+CREATE TABLE Stat
+(idStat int not null AUTO_INCREMENT,
+score int,
+idUtilisateur int not null,
+idModule int not null ,
+primary key (idStat),
+foreign key (idUtilisateur) references Utilisateur(idUtilisateur),
+foreign key (idModule) references Module(idModule));
 
-CREATE TABLE IF NOT EXISTS suggestion(
-						sug_id INT PRIMARY KEY AUTO_INCREMENT,
-						sug_text VARCHAR(255),
-						q_id INT,
-						CONSTRAINT fk_suggestion_q FOREIGN KEY(q_id) REFERENCES question(q_id)
-						) ENGINE = InnoDB;
+CREATE TABLE Reponse
+(idReponse int not null AUTO_INCREMENT,
+libelleReponse varchar(100) not null,
+isTrue boolean,
+idQuestion int not null ,
+primary key (idReponse),
+foreign key (idQuestion) references Question(idQuestion));
 
-CREATE TABLE IF NOT EXISTS answer(
-						ans_id INT PRIMARY KEY AUTO_INCREMENT,
-						q_id INT,
-						sug_id INT,
-						CONSTRAINT fk_answer_q FOREIGN KEY(q_id) REFERENCES question(q_id),
-						CONSTRAINT fk_answer_sug FOREIGN KEY(sug_id) REFERENCES suggestion(sug_id)
-						) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS progress(
-						prog_id INT PRIMARY KEY AUTO_INCREMENT,
-						prog_dt DATETIME,
-						user_id INT,
-						mod_id INT,
-						q_id INT,
-						prog_result INT,
-						CONSTRAINT fk_progress_user FOREIGN KEY(user_id) REFERENCES user(user_id),
-						CONSTRAINT fk_progress_mod FOREIGN KEY(mod_id) REFERENCES module(mod_id),
-						CONSTRAINT fk_progress_q FOREIGN KEY(q_id) REFERENCES question(q_id)
-						) ENGINE = InnoDB;
+CREATE TABLE ReponseUtilisateur
+(idUtilisateur int not null,
+idReponse int not null ,
+primary key(idUtilisateur, idReponse),
+foreign key (idReponse) references Reponse(idReponse),
+foreign key (idUtilisateur) references Utilisateur(idUtilisateur));
