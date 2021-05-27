6f885e31049d2637a611c8f7e5b6dadc90ec5352
gfayet69 <84783502+gfayet69@users.noreply.github.com>
2021-05-27 09:30:30 +0200

    Update mentionsLegales.php

--- mentionsLegales.php
+++ mentionsLegales.php
@@ -20,18 +20,18 @@
 
 <div id="blog">
 <h1 class="center" style="font-size: 35px;">MENTIONS LEGALES</h1>
-<p>
+<p style="text-align:center;">
 Les informations recueillies sur ce formulaire sont enregistrées dans un fichier informatisé par L'Equipe ProjetQuizz2 pour la soutenance du projet. 
 
 Les données collectées seront communiquées aux seuls destinataires suivants : administrateur de notre base de données.
 
-Les données sont conservées pendant 2 semaines.
+Les données sont conservées pendant 2 semaines.</br>
 
 Vous pouvez accéder aux données vous concernant, les rectifier, demander leur effacement ou exercer votre droit à la limitation du traitement de vos données. (en fonction de la base légale du traitement, mentionner également : Vous pouvez retirer à tout moment votre consentement au traitement de vos données ; Vous pouvez également vous opposer au traitement de vos données ; Vous pouvez également exercer votre droit à la portabilité de vos données)
 
 Consultez le site cnil.fr pour plus d’informations sur vos droits.
 
-Pour exercer ces droits ou pour toute question sur le traitement de vos données dans ce dispositif, vous pouvez contacter (le cas échéant, notre délégué à la protection des données ou le service chargé de l’exercice de ces droits) : CNAM Saint-Etienne, 24 rue de Robinson - 42100 Saint-Etienne.
+Pour exercer ces droits ou pour toute question sur le traitement de vos données dans ce dispositif, vous pouvez contacter (le cas échéant, notre délégué à la protection des données ou le service chargé de l’exercice de ces droits) : </br>CNAM Saint-Etienne, 24 rue de Robinson - 42100 Saint-Etienne.</br>
 
 Si vous estimez, après nous avoir contactés, que vos droits « Informatique et Libertés » ne sont pas respectés, vous pouvez adresser une réclamation à la CNIL.
 </p>
